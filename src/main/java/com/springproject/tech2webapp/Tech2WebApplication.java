106e6b4d58c4039420a789a1c0cc79961222f090
xMonox <jesper.reimert@gmail.com>
2017-12-07 14:50:29 +0100

    application change

--- src/main/java/com/springproject/tech2webapp/Tech2WebApplication.java
+++ src/main/java/com/springproject/tech2webapp/Tech2WebApplication.java
@@ -1,16 +1,12 @@
 package com.springproject.tech2webapp;
 
-import com.springproject.tech2webapp.controller.IndexController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses=IndexController.class)
 public class Tech2WebApplication extends SpringBootServletInitializer {
-
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
